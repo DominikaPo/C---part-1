b0f45c22a0be72f5b7a40b003bac326df807eb19
DominikaPo <116868602+DominikaPo@users.noreply.github.com>
2022-11-11 11:39:18 +0100

    Add files via upload

---
+++ BMI.cpp
@@ -0,0 +1,28 @@
+//Write a program that prompts for metric weight and height and outputs the BMI with two decimal places.
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    
+    double weight, height;
+    double BMI, wzrost;
+    
+    cout<< "Please enter weight in kilograms: "<<endl;
+    cin >> weight;
+    
+    cout<< "Please enter height in meters: "<<endl;
+    cin >> height;
+    
+    wzrost = height*height;
+    BMI = weight / wzrost;
+    
+    cout<< fixed<<endl;
+    cout<< setprecision(2)<<endl;
+    cout<< "BMI is: " << BMI <<endl;
+    
+    return 0;
+}
+
